e12411dee22bd367d913b154bc01fa4634b55540
maneesh888 <maneesh@oxyten.com>
2015-03-26 12:00:05 +0530

    Initial Commit

---
+++ GitSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitSample
+//
+//  Created by Maneesh M on 26/03/15.
+//  Copyright (c) 2015 Maneesh M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitSample
+//
+//  Created by Maneesh M on 26/03/15.
+//  Copyright (c) 2015 Maneesh M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
